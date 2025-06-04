426987f8b1abbdf95568cfcd82356db53fd0ec9b
William Westlake <wwestlake@lagdaemon.com>
2025-06-03 21:07:19 -0400

    deploy web

--- KwestKarz-Portal/vite.config.js
+++ KwestKarz-Portal/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   css: {
     postcss: path.resolve(__dirname, './postcss.config.cjs'),
