6f4783d8096cd1249e46cf1a35213f5bea050f5c
William Westlake <wwestlake@lagdaemon.com>
2025-06-05 17:38:38 -0400

    ui and backend updates for account invites

--- KwestKarz-Portal/src/config.js
+++ KwestKarz-Portal/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://localhost:7102',
+    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://localhost:7102/api',
   };
   
   export default config;
